9c8d5dc529c65f99d5c8338539db15ea0da88c1d
park0503 <hooo0503@gmail.com>
2022-01-24 00:43:37 +0900

    baekjoon - 1929

--- solved.ac/class2/1874.py
+++ solved.ac/class2/1874.py
@@ -2,26 +2,26 @@ def main():
     n = int(input())
     goal = []
     answer = []
-    status = []
-    current = 1
+    current = []
+    next = 1
     result = True
     for i in range(n):
         goal.append(int(input()))
     for num in goal:
-        if not status or status[-1] < num:
-            if current > num:
+        if not current or current[-1] < num:
+            if next > num:
                 result = False
                 print("NO")
                 break
-            for i in range(current, num+1):
-                status.append(current)
-                current += 1
+            for i in range(next, num+1):
+                current.append(next)
+                next += 1
                 answer.append("+")
             answer.append("-")
-            status.pop()
-        elif num <= status[-1]:
-            while status and num <= status[-1]:
-                status.pop()
+            current.pop()
+        elif num <= current[-1]:
+            while current and num <= current[-1]:
+                current.pop()
                 answer.append("-")
     if result:
         for oper in answer:
--- solved.ac/class2/1929.py
+++ solved.ac/class2/1929.py
@@ -1,19 +1,16 @@
 def main():
     start, end = map(int, input().split())
     numbers = {}
-    for i in range(0, end + 1):
+    for i in range(1, end + 1):
         if i not in numbers:
-            if i > 1:
-                numbers[i] = True
-            else:
-                numbers[i] = False
-        if numbers[i]:
+            numbers[i] = True
+        if i > 1 and numbers[i]:
             p = 2
             while p * i <= end:
                 numbers[p * i] = False
                 p += 1
     numbers = sorted(list(numbers.items()), key=lambda x: x[0])
-    for i in range(start, end + 1):
+    for i in range(start, end):
         if numbers[i][1]:
             print(numbers[i][0])
 
