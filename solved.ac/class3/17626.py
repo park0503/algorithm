04f7379caa84ed22488af7f0f82542ac7605503c
park0503 <hooo0503@gmail.com>
2022-03-04 17:12:50 +0900

    baekjoon - 17626 깔끔하게

--- solved.ac/class3/17626.py
+++ solved.ac/class3/17626.py
@@ -5,21 +5,15 @@ def main():
     n = int(sys.stdin.readline())
     dp = [0 for _ in range(n + 1)]
     dp[1] = 1
-    power = [0]
-    maxi = 1
-    while maxi ** 2 <= n:
-        power.append(maxi ** 2)
-        maxi += 1
-    power.append(maxi ** 2)
     for j in range(2, n + 1):
         i = 1
         smallest = 5
-        while j > power[i]:
-            candy = dp[j - power[i]] + 1
+        while j > i ** 2:
+            candy = dp[j - i ** 2] + 1
             if candy < smallest:
                 smallest = candy
             i += 1
-        if power[i] == j:
+        if i ** 2 == j:
             dp[j] = 1
             continue
         dp[j] = smallest
