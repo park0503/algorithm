0b097e749f36bd14d4cb8115a85f40ce1d635daa
park0503 <hooo0503@gmail.com>
2022-03-06 14:48:53 +0900

    baekjoon - 9019

--- solved.ac/class3/9019.py
+++ solved.ac/class3/9019.py
@@ -1,15 +1,37 @@
-from atexit import register
 import sys
+from collections import deque
 
 t = int(sys.stdin.readline())
-
-
-def dslr(a, b):
-    register = list(a)
-    n = int(a)
-    result = ""
-
+results = []
 
 for _ in range(t):
-    a, b = sys.stdin.readline().split()
-    print(dslr(a, b))
+    a, b = map(int, sys.stdin.readline().split())
+    stack = deque()
+    visited = set()
+    result = ""
+    while a != b:
+        d = a * 2 % 10000 if a * 2 > 9999 else a * 2
+        s = 9999 if a == 0 else a - 1
+        l = a % 1000 * 10 + a // 1000
+        r = a % 10 * 1000 + a // 10
+        if d not in visited:
+            stack.append((d, result + "D"))
+            visited.add(d)
+        if s not in visited:
+            stack.append((s, result + "S"))
+            visited.add(s)
+        if l not in visited:
+            stack.append((l, result + "L"))
+            visited.add(l)
+        if r not in visited:
+            stack.append((r, result + "R"))
+            visited.add(r)
+        if stack:
+            temp = stack.popleft()
+            a, result = temp[0], temp[1]
+        else:
+            break
+    results.append(result)
+
+for result in results:
+    print(result)
